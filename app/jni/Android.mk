0ac37d112395d013ab4fe444cb81e59b666cac65
Alynx Zhou <alynx.zhou@gmail.com>
2022-06-03 20:24:00 +0800

    Updated to disable HarfBuzz support of SDL2_ttf to reduce app size.

--- app/jni/Android.mk
+++ app/jni/Android.mk
@@ -1,1 +1,5 @@
+# SD2_ttf after 2.0.18 starts to build internal copy of HarfBuzz by default, but we don't need it.
+# So disable HarfBuzz here to reduce app size.
+SUPPORT_HARFBUZZ := false
+
 include $(call all-subdir-makefiles)
